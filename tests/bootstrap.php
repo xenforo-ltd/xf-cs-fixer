30cdbe2e5819654068554156be909354043e557e
Jeremy P <git@jerpr.com>
2026-07-06 20:40:54 -0700

    revert: "test: Only register vendor namespace if it already exists"
    This reverts commit b19ea7df2d210c76ecc6a093c4e833b7bec54d1b. This change was silly as testing is broken either way if the directory doesn't exist.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,8 +7,7 @@ use Composer\Autoload\ClassLoader;
 /** @var ClassLoader */
 $autoloader = require __DIR__ . '/../vendor/autoload.php';
 
-$phpCsFixerTestsPath = __DIR__ . '/../vendor/friendsofphp/php-cs-fixer/tests/';
-if (is_dir($phpCsFixerTestsPath))
-{
-	$autoloader->addPsr4('PhpCsFixer\\Tests\\', $phpCsFixerTestsPath);
-}
+$autoloader->addPsr4(
+	'PhpCsFixer\\Tests\\',
+	__DIR__ . '/../vendor/friendsofphp/php-cs-fixer/tests/'
+);
